0bbfb479a9557dfe026e8eee8a5619c5a88fde11
Alice088 <alien280579@gmail.com>
2024-02-21 20:38:37 +0300

    [*-] fix bugs

--- database/migrations/2024_02_04_134027_create_users_table.php
+++ database/migrations/2024_02_04_134027_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
             $table->string("USERNAME", 20);
             $table->longText("PASSWORD");
             $table->string("EMAIL", 40);
-            $table->longText("REMEMBER_TOKEN")->unique()->nullable();
+            $table->string("REMEMBER_TOKEN",  1000)->unique('remember_token_unique')->nullable();
         });
     }
 
