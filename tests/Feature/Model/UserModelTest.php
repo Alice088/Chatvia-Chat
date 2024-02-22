8cb995c309406935d758a43d591fe25d0abbdde4
Alice088 <alien280579@gmail.com>
2024-02-22 18:30:11 +0300

    [*] Update Test for User model

--- tests/Feature/Model/UserModelTest.php
+++ tests/Feature/Model/UserModelTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\User;
 use Illuminate\Support\Facades\Schema;
-use Database\Factories\UserFactory;
+use DB;
 
 class UserModelTest extends TestCase
 {
@@ -15,17 +15,16 @@ class UserModelTest extends TestCase
     public function test_add(): void
     {
         Schema::connection('mysql_test');
-        $username = fake()->name();
-        $email    = fake()->unique()->safeEmail();
-        $password = fake()->unique()->password(10, 40);
 
+        DB::beginTransaction();
         $addResult = User::add([
-            "USERNAME" => $username,
-            "EMAIL"    => $email,
-            "PASSWORD" => $password,
+            "USERNAME" => "Dr. Test",
+            "EMAIL"    => fake()->unique()->safeEmail(),
+            "PASSWORD" => fake()->unique()->password(10, 40),
         ]);
+        DB::commit();
 
-        $this->assertFalse($addResult["ERROR"]);
+        $this->assertFalse($addResult["ERROR"], $addResult["ERROR_MESSAGE"] ?? '');
         $this->assertDatabaseHas('users', [
             "ID" => $addResult["ID"],
         ]);
@@ -34,19 +33,87 @@ class UserModelTest extends TestCase
     public function test_delete(): void
     {
         Schema::connection('mysql_test');
-        $username = fake()->name();
-        $email    = fake()->unique()->safeEmail();
-        $password = fake()->unique()->password(10, 40);
 
+        DB::beginTransaction();
         $addResult = User::add([
-            "USERNAME" => $username,
-            "EMAIL"    => $email,
-            "PASSWORD" => $password,
+            "USERNAME" => "Dr. Test",
+            "EMAIL"    => fake()->unique()->safeEmail(),
+            "PASSWORD" => fake()->unique()->password(10, 40),
         ]);
-        
+        DB::commit();
+
+        DB::beginTransaction();
         $deleteResult = User::deleteUser($addResult["ID"]);
-        
-        $this->assertFalse($addResult["ERROR"]);
-        $this->assertFalse($deleteResult["ERROR"]);
+        DB::commit();
+
+        $this->assertFalse($addResult["ERROR"],  $addResult["ERROR_MESSAGE"] ?? '');
+        $this->assertFalse($deleteResult["ERROR"],  $deleteResult["ERROR_MESSAGE"] ?? '');
+    }
+
+    public function test_getBy(): void
+    {
+        Schema::connection('mysql_test');
+
+        DB::beginTransaction();
+        $addResult = User::add([
+            "USERNAME" => "Dr. Test",
+            "EMAIL"    => fake()->unique()->safeEmail(),
+            "PASSWORD" => fake()->unique()->password(10, 40),
+        ]);
+
+        $getResult = User::getBy("ID", "=", $addResult["ID"]);
+        DB::commit();
+
+        $this->assertFalse($getResult["ERROR"],  $getResult["ERROR_MESSAGE"] ?? '');
+        $this->assertTrue($addResult["ID"] === $getResult["USER"]->ID);
+        $this->assertFalse($addResult["ERROR"],  $addResult["ERROR_MESSAGE"] ?? '');
+    }
+
+    public function test_editPassword(): void
+    {
+        Schema::connection('mysql_test');
+        $newPassword = app("PasswordManager")::createRandomPassword();
+
+        DB::beginTransaction();
+        $addResult = User::add([
+            "USERNAME" => "Dr. Test",
+            "EMAIL"    => fake()->unique()->safeEmail(),
+            "PASSWORD" => fake()->unique()->password(10, 40)
+        ]);
+        DB::commit();
+
+        DB::beginTransaction();
+        $editResult = User::editPassword($addResult["ID"], $newPassword);
+        $getResult = User::getBy("ID", "=", $addResult["ID"]);
+        DB::commit();
+
+        $this->assertFalse($getResult["ERROR"], $getResult["ERROR_MESSAGE"] ?? '');
+        $this->assertFalse($editResult["ERROR"],  $editResult["ERROR_MESSAGE"] ?? '');
+        $this->assertTrue(password_verify($newPassword, $getResult["USER"]->PASSWORD));
+        $this->assertFalse($addResult["ERROR"], $addResult["ERROR_MESSAGE"] ?? '');
+    }
+
+    public function test_updateRememberToken(): void
+    {
+        Schema::connection('mysql_test');
+
+        DB::beginTransaction();
+        $addResult = User::add([
+            "USERNAME" => "Dr. Test",
+            "EMAIL"    => fake()->unique()->safeEmail(),
+            "PASSWORD" => fake()->unique()->password(10, 40)
+        ]);
+        DB::commit();
+
+        DB::beginTransaction();
+        $getResult = User::getBy("ID", "=", $addResult["ID"]);
+        $updateResult = User::updateRememberToken($addResult["ID"]);
+        DB::commit();
+
+
+        $this->assertFalse($getResult["ERROR"],  $getResult["ERROR_MESSAGE"] ?? '');
+        $this->assertFalse($updateResult["ERROR"], $updateResult["ERROR_MESSAGE"] ?? '');
+        $this->assertTrue($getResult["USER"]->REMEMBER_TOKEN !== $updateResult["REMEMBER_TOKEN"]);
+        $this->assertFalse($addResult["ERROR"], $addResult["ERROR_MESSAGE"] ?? '');
     }
 }
